824c3388ded08f2a634ef4e914a804a0b764acaf
Joselyne22HR <joselineodalish@gmail.com>
2022-07-08 12:27:11 -0500

    init

--- src/student/entities/student.schema.ts
+++ src/student/entities/student.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export type StudentDocument = Student & Document;
-
+//
 @Schema()
 export class Student {
   @Prop()
